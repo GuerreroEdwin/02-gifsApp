966e89cddb7ce36ea5bd851aacd1d9c12897764e
Edwin Guerrero <guerreroedwinarmando@gmail.com>
2022-05-14 23:45:41 -0500

    capitulo 86

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,31 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
-  constructor() { }
+  private apiKeY    : string = 'hwvh47AwjaSs7iNg0pDgvCTPCoAqXyqQ';
+  private _historial: string [] = [];
+
+  public resultados: any [] = [];
+
+  get historial() { 
+    
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient){}
+  buscarGifs(query: string = '') {
+
+    query = query.trim().toLocaleLowerCase();    
+
+    if (!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+        }
+   
+        this.http.get(`http://api.giphy.com/v1/gifs/search?api_key=hwvh47AwjaSs7iNg0pDgvCTPCoAqXyqQ&q=${query}Z&limit=10`)
+        .subscribe((resp: any) =>{
+          console.log(resp.data);
+          this.resultados = resp.data;
+        });
+    
+  }
 }
