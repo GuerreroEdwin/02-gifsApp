44f4af6336931ab4d705367614e888ff779db23d
Edwin Guerrero <guerreroedwinarmando@gmail.com>
2022-05-15 18:41:39 -0500

    capitulo 88

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../inteface/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,24 @@ export class GifsService {
   private apiKeY    : string = 'hwvh47AwjaSs7iNg0pDgvCTPCoAqXyqQ';
   private _historial: string [] = [];
 
-  public resultados: any [] = [];
+  public resultados: Gif   [] = [];
 
   get historial() { 
     
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+
+    /* if(localStorage.getItem('historial')){
+      this._historial = JSON.parse(localStorage.getItem('historial')!);
+    } */
+  }
+
+
+
   buscarGifs(query: string = '') {
 
     query = query.trim().toLocaleLowerCase();    
@@ -24,10 +35,14 @@ export class GifsService {
     if (!this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    
+
         }
    
-        this.http.get(`http://api.giphy.com/v1/gifs/search?api_key=hwvh47AwjaSs7iNg0pDgvCTPCoAqXyqQ&q=${query}Z&limit=10`)
-        .subscribe((resp: any) =>{
+        this.http.get <SearchGifsResponse>(`http://api.giphy.com/v1/gifs/search?api_key=hwvh47AwjaSs7iNg0pDgvCTPCoAqXyqQ&q=${query}Z&limit=10`)
+        .subscribe(( resp ) =>{
           console.log(resp.data);
           this.resultados = resp.data;
         });
